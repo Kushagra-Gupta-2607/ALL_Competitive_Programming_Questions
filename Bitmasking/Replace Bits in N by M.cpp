caa3702b0f63c457250e18f92dfb6222d1cd7465
Kushagra-Gupta-2607 <98476315+Kushagra-Gupta-2607@users.noreply.github.com>
2022-02-05 15:27:33 +0530

    Update and rename Unique Number to Replace Bits in N by M.cpp

---
+++ Bitmasking/Replace Bits in N by M.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Replace Bits in N by M
+
+// Problem - 
+// You are given 2 32-bit numbers (N & M), and 2 bit positions (i & j).
+// Write a method to set all bits b/w i & j in N equal to M.
+// (i.e. M becomes a substring of N starting from i and ending at j)
+
+// Example -
+// Input:  N = 10000000000, M = 10101, i = 2, j = 6
+// Output: N = 10001010100
+
+int main(){
+
+     
+
+
+    return 0;
+}
