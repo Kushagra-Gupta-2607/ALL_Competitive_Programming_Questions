01a4058fe8afca763e837521c8a592a503a6cc40
Kushagra-Gupta-2607 <98476315+Kushagra-Gupta-2607@users.noreply.github.com>
2022-02-07 15:19:32 +0530

    Create Big Facorial.java

---
+++ Big Integers/Big Facorial.java
@@ -0,0 +1,37 @@
+// Big Integer Class in Java | Powerful lot of inbuilt functions
+/*
+Modular Arithmetic
+GCD Calculation
+Base Conversion
+Power Calculation
+Bitwise Operators
+Prime Generation
+*/
+
+/*--------------------Code--------------------*/
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+    
+    static BigInteger BigFactorial(int n){
+        
+        BigInteger b = new BigInteger("1");
+        for(int i = 2; i<=n; i++){
+            b = b.multiply(BigInteger.valueOf(i)); 
+            // i should also be converted to BigInteger first
+        }
+        return b;
+        
+    }
+    
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        System.out.println(BigFactorial(n));
+        
+    }
+    
+}
